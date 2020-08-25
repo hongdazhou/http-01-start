3df54390a55325016ae98265c697156aae683931
hongdazhou <zhouhongda@gmail.com>
2020-08-25 09:16:43 -0400

    h1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,35 @@
-// import { Component, OnInit } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent implements OnInit {
-//   loadedPosts = [];
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
 
-//   constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-//   ngOnInit() {}
+  ngOnInit() {}
 
-//   onCreatePost(postData: { title: string; content: string }) {
-//     // Send Http request
-//     this.http
-//       .post(
-//         'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
-//         postData
-//       )
-//       .subscribe(responseData => {
-//         console.log(responseData);
-//       });
-//   }
+  // onCreatePost(postData: { title: string; content: string }) {
+  //   // Send Http request
+  //   this.http
+  //     .post(
+  //       'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+  //       postData
+  //     )
+  //     .subscribe(responseData => {
+  //       console.log(responseData);
+  //     });
+  // }
 
-//   onFetchPosts() {
-//     // Send Http request
-//   }
+  onFetchPosts() {
+    // Send Http request
+  }
 
-//   onClearPosts() {
-//     // Send Http request
-//   }
-// }
+  onClearPosts() {
+    // Send Http request
+  }
+}
